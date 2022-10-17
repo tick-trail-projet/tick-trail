e7998d7aba161c3d91fd968a924e0f29829bd4e4
Steeddred <84187389+Steeddred@users.noreply.github.com>
2022-10-18 00:03:18 +0200

    Add files via upload

---
+++ Resrvation Tickets/Exec.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+import java.util.Locale;
+
+public class Exec  {
+
+	public static void main(String[] args)   {
+		Ticket t1 = new Ticket("669","06/03/22","Orleans - Paris",32.50 , 0);
+		
+		System.out.println(t1.ImpTicket());
+		
+		
+	
+	
+		
+		
+		  
+		  
+	}
+
+}

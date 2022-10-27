c5d0461b0a554b518bd2c9611bfd0548c9135ffc
grality <leodiscord41@gmail.com>
2022-10-27 20:35:54 +0200

    JAVA FX test

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stg = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("navBar.fxml"));
+        primaryStage.setTitle("Tick&Trail");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        Horaires test = new Horaires(14, 10, 2022, "21:23");
-        System.out.println(test.check());
-    }
-}

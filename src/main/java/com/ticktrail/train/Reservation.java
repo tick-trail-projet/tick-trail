0586bafa8442a5baf8853b7a7dd40f4b8b1588dc
grality <leodiscord41@gmail.com>
2022-12-11 13:33:47 +0100

    edit

--- src/main/java/com/ticktrail/train/Reservation.java
+++ src/main/java/com/ticktrail/train/Reservation.java
@@ -12,40 +12,41 @@ import com.ticktrail.database.Mysql;
 import com.ticktrail.user.User;
 
 /**
- * classe gerant les interactions entre le lien reservation d'un trajet/utilisateur et la BDD
+ * classe gerant les interactions entre le lien reservation d'un
+ * trajet/utilisateur et la BDD
  */
 public class Reservation extends Mysql {
-	
-	/**
-	 * constructeur
-	 */
+
+    /**
+     * constructeur
+     */
     public Reservation() {
         super();
     }
-	
-	/**
-	 * AJout d'un lien reservation d'un trajet/utilisateur en BDD
-	 *
-	 * @param train_id identifiant du trajet
-	 * @throws IOException Si une erreur de lecture/ecriture arrive
-	 */
+
+    /**
+     * AJout d'un lien reservation d'un trajet/utilisateur en BDD
+     *
+     * @param train_id identifiant du trajet
+     * @throws IOException Si une erreur de lecture/ecriture arrive
+     */
     public void addReservation(int train_id) throws IOException {
         User user = new User();
         super.runQuery(
                 "INSERT INTO `reservations`(`user_id`,`train_id`) VALUES ('" + user.getWithToken().get("id") + "','"
                         + train_id + "')");
     }
-	
-	/**
-	 * suppression d'un lien reservation d'un trajet/utilisateur en BDD
-	 *
-	 * @param from origine
-	 * @param to destination
-	 * @param price prix
-	 * @param from_schedule heure de depart
-	 * @param to_schedule heure d arrivee
-	 * @throws IOException Si une erreur de lecture/ecriture arrive
-	 */
+
+    /**
+     * suppression d'un lien reservation d'un trajet/utilisateur en BDD
+     *
+     * @param from          origine
+     * @param to            destination
+     * @param price         prix
+     * @param from_schedule heure de depart
+     * @param to_schedule   heure d arrivee
+     * @throws IOException Si une erreur de lecture/ecriture arrive
+     */
     public void deleteReservation(String from, String to, int price, String from_schedule, String to_schedule)
             throws IOException {
         User user = new User();
@@ -53,18 +54,18 @@ public class Reservation extends Mysql {
                 + "\" AND to_schedule = \"" + to_schedule + "\" AND user_id = \""
                 + user.getWithToken().get("id") + "\"");
     }
-	
-	/**
-	 * simulation d'une liste de trajets disponibles
-	 *
-	 * @param from origine
-	 * @param to destination
-	 * @param from_schedule heure de depart
-	 * @return list de trajet disponible
-	 */
+
+    /**
+     * simulation d'une liste de trajets disponibles
+     *
+     * @param from          origine
+     * @param to            destination
+     * @param from_schedule heure de depart
+     * @return list de trajet disponible
+     */
     public ArrayList<Trip> simulation(String from, String to, String from_schedule) {
         double number = randomNumber(2, 10);
-        ArrayList<Trip> list = new ArrayList<Trip>();
+        ArrayList<Trip> list = new ArrayList<>();
         for (int i = 0; i < number; i++) {
             Cities city_from = new Cities(from);
             Cities city_to = new Cities(to);
@@ -83,28 +84,28 @@ public class Reservation extends Mysql {
         }
         return list;
     }
-	
-	/**
-	 * generation d'un nombre aleatoire entre 2 valeurs
-	 *
-	 * @param min valeur min
-	 * @param max valeur max
-	 * @return le nombre aleatoire
-	 */
+
+    /**
+     * generation d'un nombre aleatoire entre 2 valeurs
+     *
+     * @param min valeur min
+     * @param max valeur max
+     * @return le nombre aleatoire
+     */
     public int randomNumber(int min, int max) {
         Random random = new Random();
         return random.nextInt(max + 1 - min) + min;
     }
-	
-	/**
-	 * retourne la liste des trajets d'un utilisateur
-	 *
-	 * @return la liste des trajets d'un utilisateur
-	 * @throws IOException Si une erreur de lecture/ecriture arrive
-	 * @throws SQLException exception liée a la BDD
-	 */
+
+    /**
+     * retourne la liste des trajets d'un utilisateur
+     *
+     * @return la liste des trajets d'un utilisateur
+     * @throws IOException  Si une erreur de lecture/ecriture arrive
+     * @throws SQLException exception liée a la BDD
+     */
     public ArrayList<Trip> MyReservation() throws IOException, SQLException {
-        ArrayList<Trip> list = new ArrayList<Trip>();
+        ArrayList<Trip> list = new ArrayList<>();
         User user = new User();
         ResultSet rs = super.getQuery(
                 "SELECT * FROM trips WHERE user_id = \"" + user.getWithToken().get("id") + "\" ORDER BY id DESC");
